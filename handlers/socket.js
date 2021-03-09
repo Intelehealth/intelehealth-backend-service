e0bbfe5db94a20b7b37b72c933f8763c7d5c820a
Zeeshan <zeeshan@intelehealth.org>
2021-03-09 16:42:55 +0530

    socket tweak for webRTC

--- handlers/socket.js
+++ handlers/socket.js
@@ -27,49 +27,4 @@ module.exports = function (server) {
       io.to(data.to).emit("callAccepted", data.signal);
     });
   });
-
-  // io.on("connection", function (client) {
-  //   console.log("-- " + client.id + " joined --");
-  //   client.emit("id", client.id);
-  //   streams.addStream(client.id);
-  //   io.sockets.emit("activeUsers", {
-  //     users: streams.getStreams(),
-  //   });
-
-  //   client.on("candidate", (data) => {
-  //     console.log("data: ", data);
-  //     io.to(data.to).emit("new-candidate", {
-  //       id: client.id,
-  //       candidate: data.candidate,
-  //     });
-  //   });
-
-  //   client.on("answer", (data) => {
-  //     io.to(data.to).emit("answer-made", {
-  //       id: client.id,
-  //       answer: data.answer,
-  //     });
-  //   });
-
-  //   client.on("call", function (options) {
-  //     console.log("-- " + client.id + " is ready to stream --");
-  //     io.to(options.to).emit("calling", {
-  //       offer: options.offer,
-  //       id: client.id,
-  //     });
-  //   });
-
-  //   client.on("update", function (options) {
-  //     streams.update(client.id, options.name);
-  //   });
-
-  //   function leave() {
-  //     console.log("-- " + client.id + " left --");
-  //     streams.removeStream(client.id);
-  //   }
-
-  //   client.on("disconnect", leave);
-  //   client.on("leave", leave);
-  // });
-  // global.streams = new Stream();
 };
