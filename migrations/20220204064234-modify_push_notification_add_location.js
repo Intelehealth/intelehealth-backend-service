6d32adcb53f65d44fc1631f017e89aee191e9b53
Zeeshan <zeeshan@intelehealth.org>
2022-04-29 15:59:16 +0530

    locale in pushnotification

--- migrations/20220204064234-modify_push_notification_add_location.js
+++ migrations/20220204064234-modify_push_notification_add_location.js
@@ -2,12 +2,21 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.addColumn("pushnotification", "location", {
-      type: Sequelize.STRING,
-    });
+    return;
+    return Promise.all([
+      queryInterface.addColumn("pushnotification", "location", {
+        type: Sequelize.STRING,
+      }),
+      queryInterface.addColumn("pushnotification", "locale", {
+        type: Sequelize.STRING,
+      }),
+    ]);
   },
 
   down: async (queryInterface, Sequelize) => {
-    return queryInterface.removeColumn("pushnotification", "location");
+    return Promise.all([
+      queryInterface.removeColumn("pushnotification", "location"),
+      queryInterface.removeColumn("pushnotification", "locale"),
+    ]);
   },
 };
