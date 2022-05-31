329a9b154b356ef1393e69791e0bb46d994fedc9
Zeeshan <zeeshan@intelehealth.org>
2022-05-31 17:03:51 +0530

    EZ-19 nurse to doctor call and refactoring

--- handlers/socket.js
+++ handlers/socket.js
@@ -40,12 +40,12 @@ module.exports = function (server) {
       log("Room " + room + " now has " + numClients + " client(s)");
       socket.on("message", function (message) {
         log("Client said: ", message);
-        io.sockets.in(room).emit("message", message);
+        socket.broadcast.to(room).emit("message", message);
+        // io.sockets.in(room).emit("message", message);
       });
 
       socket.on("bye", function (data) {
         console.log("received bye");
-        io.sockets.in(room).emit("message", "bye");
         io.sockets.in(room).emit("bye");
         io.sockets.emit("log", ["received bye", data]);
       });
@@ -58,95 +58,98 @@ module.exports = function (server) {
 
       console.log("numClients: ", numClients);
       if (numClients === 0) {
+        // socket.broadcast.to(room).emit("incoming_call", { patientUuid: room });
         io.sockets.emit("incoming_call", { patientUuid: room });
         socket.join(room);
         log("Client ID " + socket.id + " created room " + room);
         socket.emit("created", room, socket.id);
       } else if (numClients === 1) {
         log("Client ID " + socket.id + " joined room " + room);
-        io.sockets.in(room).emit("join", room);
+        // io.sockets.broadcast.to(room).emit("join", room);
+        // io.sockets.in(room).emit("join", room);
         socket.join(room);
         socket.emit("joined", room, socket.id);
-        io.sockets.in(room).emit("ready");
+        socket.broadcast.to(room).emit("ready");
+        // io.sockets.in(room).emit("ready");
       } else {
         socket.emit("full", room);
       }
     });
 
-    socket.on("create or join", function (room) {
-      log("Received request to create or join room " + room);
+    // socket.on("create or join", function (room) {
+    //   log("Received request to create or join room " + room);
 
-      var clientsInRoom = io.sockets.adapter.rooms[room];
-      var numClients = clientsInRoom
-        ? Object.keys(clientsInRoom.sockets).length
-        : 0;
-      log("Room " + room + " now has " + numClients + " client(s)");
-      socket.on("message", function (message) {
-        log("Client said: ", message);
-        io.sockets.in(room).emit("message", message);
-      });
+    //   var clientsInRoom = io.sockets.adapter.rooms[room];
+    //   var numClients = clientsInRoom
+    //     ? Object.keys(clientsInRoom.sockets).length
+    //     : 0;
+    //   log("Room " + room + " now has " + numClients + " client(s)");
+    //   socket.on("message", function (message) {
+    //     log("Client said: ", message);
+    //     io.sockets.in(room).emit("message", message);
+    //   });
 
-      socket.on("bye", function (data) {
-        console.log("received bye");
-        io.sockets.in(room).emit("message", "bye");
-        io.sockets.in(room).emit("bye");
-        io.sockets.emit("log", ["received bye", data]);
-      });
+    //   socket.on("bye", function (data) {
+    //     console.log("received bye");
+    //     io.sockets.in(room).emit("message", "bye");
+    //     io.sockets.in(room).emit("bye");
+    //     io.sockets.emit("log", ["received bye", data]);
+    //   });
 
-      socket.on("no_answer", function (data) {
-        console.log("no_answer");
-        io.sockets.in(room).emit("bye");
-        io.sockets.emit("log", ["no_answer", data]);
-      });
+    //   socket.on("no_answer", function (data) {
+    //     console.log("no_answer");
+    //     io.sockets.in(room).emit("bye");
+    //     io.sockets.emit("log", ["no_answer", data]);
+    //   });
 
-      if (numClients === 0) {
-        socket.join(room);
-        log("Client ID " + socket.id + " created room " + room);
-        socket.emit("created", room, socket.id);
-      } else if (numClients === 1) {
-        log("Client ID " + socket.id + " joined room " + room);
-        io.sockets.in(room).emit("join", room);
-        socket.join(room);
-        socket.emit("joined", room, socket.id);
-        io.sockets.in(room).emit("ready");
-      } else {
-        socket.emit("full", room);
-      }
-    });
-    socket.on("call", async function (dataIds) {
-      const { nurseId } = dataIds;
-      let isCalling = false;
-      for (const socketId in users) {
-        if (Object.hasOwnProperty.call(users, socketId)) {
-          const userObj = users[socketId];
-          if (userObj.uuid === nurseId) {
-            io.sockets.to(socketId).emit("call", dataIds);
-            isCalling = true;
-          }
-        }
-      }
-      if (!isCalling) {
-        const data = await user_settings.findOne({
-          where: { user_uuid: nurseId },
-        });
-        if (data && data.device_reg_token) {
-          const response = await sendCloudNotification({
-            title: "Incoming call",
-            body: "Doctor is trying to call you.",
-            data: { ...dataIds, actionType: "VIDEO_CALL" },
-            regTokens: [data.device_reg_token],
-          }).catch((err) => {
-            console.log("err: ", err);
-          });
-          io.sockets.emit("log", ["notification response", response, data]);
-        } else {
-          io.sockets.emit("log", [
-            `data/device reg token not found in db for ${nurseId}`,
-            data,
-          ]);
-        }
-      }
-    });
+    //   if (numClients === 0) {
+    //     socket.join(room);
+    //     log("Client ID " + socket.id + " created room " + room);
+    //     socket.emit("created", room, socket.id);
+    //   } else if (numClients === 1) {
+    //     log("Client ID " + socket.id + " joined room " + room);
+    //     io.sockets.in(room).emit("join", room);
+    //     socket.join(room);
+    //     socket.emit("joined", room, socket.id);
+    //     io.sockets.in(room).emit("ready");
+    //   } else {
+    //     socket.emit("full", room);
+    //   }
+    // });
+    // socket.on("call", async function (dataIds) {
+    //   const { nurseId } = dataIds;
+    //   let isCalling = false;
+    //   for (const socketId in users) {
+    //     if (Object.hasOwnProperty.call(users, socketId)) {
+    //       const userObj = users[socketId];
+    //       if (userObj.uuid === nurseId) {
+    //         io.sockets.to(socketId).emit("call", dataIds);
+    //         isCalling = true;
+    //       }
+    //     }
+    //   }
+    //   if (!isCalling) {
+    //     const data = await user_settings.findOne({
+    //       where: { user_uuid: nurseId },
+    //     });
+    //     if (data && data.device_reg_token) {
+    //       const response = await sendCloudNotification({
+    //         title: "Incoming call",
+    //         body: "Doctor is trying to call you.",
+    //         data: { ...dataIds, actionType: "VIDEO_CALL" },
+    //         regTokens: [data.device_reg_token],
+    //       }).catch((err) => {
+    //         console.log("err: ", err);
+    //       });
+    //       io.sockets.emit("log", ["notification response", response, data]);
+    //     } else {
+    //       io.sockets.emit("log", [
+    //         `data/device reg token not found in db for ${nurseId}`,
+    //         data,
+    //       ]);
+    //     }
+    //   }
+    // });
 
     socket.on("ipaddr", function () {
       var ifaces = os.networkInterfaces();
