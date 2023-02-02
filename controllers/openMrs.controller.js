279d384ab2ffe66b281cdff0f1a47599bd3206f1
dhanashri.p <dhanashri@intelehealth.org>
2023-02-02 11:52:21 +0530

    Follow up visit related API

--- controllers/openMrs.controller.js
+++ controllers/openMrs.controller.js
@@ -78,6 +78,33 @@ group by case
        end;`;
 };
 
+const getFollowUpVisitOfDr = (providerId) => {
+  return `select 	v.uuid as visit_id,
+  v.patient_id,
+      max(e.encounter_id) as visit_note_encounter,
+      max(e.creator) as doctor_id,
+      max(ep.provider_id) as provider_id,
+      max(p.uuid) as provider_uuid,
+      max(value_text) as followup_text
+from 	visit v,
+  encounter e,
+      encounter_provider ep,
+      provider p,
+      obs o
+where	v.voided = 0
+and		e.visit_id = v.visit_id
+and   p.uuid ='${providerId}'
+and		e.encounter_type = 9
+and		e.voided = 0
+and		ep.encounter_id = e.encounter_id
+and		p.provider_id = ep.provider_id
+and		o.encounter_id = e.encounter_id
+and		o.concept_id = 163345
+group by 	v.visit_id,
+    v.patient_id
+order by 4 ;`;
+};
+
 /**
  * To return the visit counts from the openmrs db using custom query
  * @param {*} req
@@ -104,6 +131,33 @@ const getVisitCounts = async (req, res, next) => {
       data,
       message: "Visit count fetched successfully",
     });
+  } catch (error) {
+    res.statusCode = 422;
+    res.json({ status: false, message: err.message });
+  }
+};
+
+/**
+ * To return the FollowUp Visits from the openmrs db using custom query
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
+const getFollowUpVisit = async (req, res, next) => {
+  const providerId = req.params.providerId;
+  const query = getFollowUpVisitOfDr(providerId);
+  try {
+    const data = await new Promise((resolve, reject) => {
+      openMrsDB.query(query, (err, results, fields) => {
+        if (err) reject(err);
+        resolve(results);
+      });
+    }).catch((err) => {
+      throw err;
+    });
+    res.json(
+      data
+    );
   } catch (error) {
     res.statusCode = 422;
     res.json({ status: false, message: err.message });
@@ -144,6 +198,7 @@ const forgetPasswordResetPassword = async (req, res, next) => {
 
 module.exports = {
   getVisitCounts,
+  getFollowUpVisit,
   forgetPasswordSendOtp,
   forgetPasswordResetPassword,
 };
