db0c58ecae5d26d5a435599b15fefa742cbdf0be
ANURAG SANGALE <anurag@intelehealth.org>
2023-03-23 12:48:34 +0530

    Updated app.js for bodyparser change

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(function (req, res, next) {
 app.use(logger("dev"));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: false }));
-app.use(bodyParser.text());
+app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
