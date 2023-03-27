86802bd68a6b87d2dd0386d1c6194db37afa1e5c
Zeeshan <zeeshan@intelehealth.org>
2023-03-27 17:27:26 +0530

    added openmrs db config

--- openmrs_models/person_name.js
+++ openmrs_models/person_name.js
@@ -1,7 +1,7 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
+const { Sequelize } = require("../openmrs_models");
+
 module.exports = (sequelize, DataTypes) => {
   class person_name extends Model {
     /**
@@ -12,31 +12,37 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
-  person_name.init({
-    person_name_id: DataTypes.INTEGER,
-    preferred: DataTypes.BOOLEAN,
-    person_id: DataTypes.INTEGER,
-    prefix: DataTypes.STRING,
-    given_name: DataTypes.STRING,
-    middle_name: DataTypes.STRING,
-    family_name_prefix: DataTypes.STRING,
-    family_name: DataTypes.STRING,
-    family_name2: DataTypes.STRING,
-    family_name_suffix: DataTypes.STRING,
-    degree: DataTypes.STRING,
-    creator: DataTypes.INTEGER,
-    date_created: DataTypes.DATE,
-    voided: DataTypes.BOOLEAN,
-    voided_by: DataTypes.INTEGER,
-    date_voided: DataTypes.DATE,
-    void_reason: DataTypes.STRING,
-    changed_by: DataTypes.INTEGER,
-    date_changed: DataTypes.DATE,
-    uuid: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'person_name',
-  });
+  }
+  person_name.init(
+    {
+      person_name_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+      },
+      preferred: DataTypes.TINYINT,
+      person_id: DataTypes.INTEGER,
+      prefix: "VARCHAR",
+      given_name: "VARCHAR",
+      middle_name: "VARCHAR",
+      family_name_prefix: "VARCHAR",
+      family_name: "VARCHAR",
+      family_name2: "VARCHAR",
+      family_name_suffix: "VARCHAR",
+      degree: "VARCHAR",
+      creator: DataTypes.INTEGER,
+      date_created: DataTypes.DATE,
+      voided: DataTypes.TINYINT,
+      voided_by: DataTypes.INTEGER,
+      date_voided: DataTypes.DATE,
+      void_reason: "VARCHAR",
+      changed_by: DataTypes.INTEGER,
+      date_changed: DataTypes.DATE,
+      uuid: DataTypes.CHAR,
+    },
+    {
+      sequelize,
+      modelName: "person_name",
+    }
+  );
   return person_name;
 };
