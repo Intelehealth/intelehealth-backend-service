0c956549f05821703654eaa99922b4e121063a64
ANURAG SANGALE <anurag@intelehealth.org>
2023-06-07 15:44:35 +0530

    Added API to store GPT Inputs

--- controllers/openai.controller.js
+++ controllers/openai.controller.js
@@ -1,17 +1,172 @@
 const { RES } = require("../handlers/helper");
-const { createCompletion } = require("../services/openai.service");
+const { createCompletion, getGTPInputs, addGTPInput, setAsDefaultGTPInput, deleteGPTInput } = require("../services/openai.service");
 
 module.exports = (function () {
     /**
-     * Request otp for forgot-username or forgot-password and sing in verification
+     * 
      * @param {*} req
      * @param {*} res
      */
     this.createCompletion = async (req, res) => {
       try {
-        const { payload } = req.body;
-        if (payload) {
-          const data = await createCompletion(payload);
+        const { payload, inputtype, customInput } = req.body;
+        if (payload && ((inputtype == 'default' && customInput == null) || (inputtype == 'custom' && customInput != null))) {
+          const data = await createCompletion(payload, inputtype, customInput);
+          RES(
+            res,
+            {
+              success: data.success,
+              message: data.message,
+              data: data.data,
+            },
+            data.code
+          );
+        } else {
+          RES(
+            res,
+            {
+              success: false,
+              message: "Bad request! Invalid arguments.",
+              data: null,
+            },
+            400
+          );
+        }
+      } catch (error) {
+        if (error.code === null || error.code === undefined) {
+          error.code = 500;
+        }
+        RES(
+          res,
+          { success: false, data: error.data, message: error.message },
+          error.code
+        );
+      }
+    };
+
+    /**
+     * 
+     * @param {*} req
+     * @param {*} res
+     */
+    this.getGTPInputs = async (req, res) => {
+      try {
+        const data = await getGTPInputs();
+        RES(
+          res,
+          {
+            success: data.success,
+            message: data.message,
+            data: data.data,
+          },
+          data.code
+        );
+      } catch (error) {
+        if (error.code === null || error.code === undefined) {
+          error.code = 500;
+        }
+        RES(
+          res,
+          { success: false, data: error.data, message: error.message },
+          error.code
+        );
+      }
+    };
+
+    /**
+     * 
+     * @param {*} req
+     * @param {*} res
+     */
+    this.addGTPInput = async (req, res) => {
+      try {
+        const { gptinput } = req.body;
+        if (gptinput) {
+          const data = await addGTPInput(gptinput);
+          RES(
+            res,
+            {
+              success: data.success,
+              message: data.message,
+              data: data.data,
+            },
+            data.code
+          );
+        } else {
+          RES(
+            res,
+            {
+              success: false,
+              message: "Bad request! Invalid arguments.",
+              data: null,
+            },
+            400
+          );
+        }
+      } catch (error) {
+        if (error.code === null || error.code === undefined) {
+          error.code = 500;
+        }
+        RES(
+          res,
+          { success: false, data: error.data, message: error.message },
+          error.code
+        );
+      }
+    };
+
+    /**
+     * 
+     * @param {*} req
+     * @param {*} res
+     */
+    this.setAsDefaultGTPInput = async (req, res) => {
+      try {
+        const { id } = req.body;
+        if (id) {
+          const data = await setAsDefaultGTPInput(id);
+          RES(
+            res,
+            {
+              success: data.success,
+              message: data.message,
+              data: data.data,
+            },
+            data.code
+          );
+        } else {
+          RES(
+            res,
+            {
+              success: false,
+              message: "Bad request! Invalid arguments.",
+              data: null,
+            },
+            400
+          );
+        }
+      } catch (error) {
+        if (error.code === null || error.code === undefined) {
+          error.code = 500;
+        }
+        RES(
+          res,
+          { success: false, data: error.data, message: error.message },
+          error.code
+        );
+      }
+    };
+
+    /**
+     * 
+     * @param {*} req
+     * @param {*} res
+     */
+    this.deleteGPTInput = async (req, res) => {
+      try {
+        const { id } = req.params;
+        if (id) {
+          const data = await deleteGPTInput(id);
           RES(
             res,
             {
--- services/openai.service.js
+++ services/openai.service.js
@@ -6,17 +6,30 @@ const configuration = new Configuration({
     apiKey: config[env].openaiApiKey,
 });
 const openai = new OpenAIApi(configuration);
+const { gptinputs, Sequelize, sequelize } = require("../models");
 
 module.exports = (function () {
 
-    this.createCompletion = async function (payload) {
+    this.createCompletion = async function (payload, inputtype, customInput) {
         try {
+            let gptInput = '';
+            if (inputtype == 'default') {
+                const data = await gptinputs.findAll({
+                    where: {
+                        isDefault: true
+                    }
+                });
+                gptInput = data.length ? data[0].gptinput : 'Provide a Differential diagnosis with likelihood-likelihood-matrix, recommended diagnostics and recommended treatment plans in a four column table format with the Likelihood numeric scores (out of 10) ranked in descending order. Keep in mind the demographic background of the patient - ';
+            }
+            if (inputtype == 'custom') {
+                gptInput = customInput;
+            }
             const response = await openai.createChatCompletion({
                 model: 'gpt-3.5-turbo',
                 messages: [
                     {
                         role: "user",
-                        content: `Provide a Differential diagnosis with likelihood-likelihood-matrix in a two column table format with Diagnosis and the Likelihood numeric score (out of 10) ranked in descending order. Keep in mind the age & gender of the patient - ${payload}`
+                        content: `${gptInput} ${payload}`
                     }
                 ]
             });
@@ -33,6 +46,120 @@ module.exports = (function () {
             return { code: error.code, success: false, data: error.data, message: error.message };
         }
         
+    };
+
+    this.getGTPInputs = async function () {
+        try {
+            const response = await gptinputs.findAll({
+                where: {
+                    id: {
+                        [Sequelize.Op.not]: null
+                    }
+                },
+                order: [
+                    ['id', 'DESC']
+                ],
+            });
+            return {
+                code: 200,
+                success: true,
+                message: "List of GPT Inputs retreived successfully!",
+                data: response
+            };
+        } catch (error) {
+            if(error.code === null || error.code === undefined){
+                error.code = 500;
+            }
+            return { code: error.code, success: false, data: error.data, message: error.message };
+        }
+    };
+
+    this.addGTPInput = async function (gptinput) {
+        try {
+            const data = await gptinputs.create({
+               gptinput
+            });
+            return {
+                code: 200,
+                success: true,
+                message: "GPT Input added successfully successfully!",
+                data: data
+            };
+        } catch (error) {
+            if(error.code === null || error.code === undefined){
+                error.code = 500;
+            }
+            return { code: error.code, success: false, data: error.data, message: error.message };
+        }
+    };
+
+    this.setAsDefaultGTPInput = async function (id) {
+        try {
+
+            const response = await gptinputs.update(
+                { isDefault: false },
+                {
+                  where: {
+                    id: {
+                        [Sequelize.Op.not] : id
+                    }
+                  }
+                }
+            );
+            const data = await gptinputs.update(
+                { isDefault: true },
+                {
+                  where: {
+                    id: id
+                  }
+                }
+            );
+            return {
+                code: 200,
+                success: true,
+                message: "GPT Input set as default successfully!",
+                data: data
+            };
+        } catch (error) {
+            if(error.code === null || error.code === undefined){
+                error.code = 500;
+            }
+            return { code: error.code, success: false, data: error.data, message: error.message };
+        }
+    };
+
+    this.deleteGPTInput = async function (id) {
+        try {
+            const data = await gptinputs.destroy(
+                {
+                  where: {
+                    id: id,
+                    isDefault: false
+                  }
+                }
+            );
+            if (data) {
+                return {
+                    code: 200,
+                    success: true,
+                    message: "GPT Input deleted successfully!",
+                    data: null
+                };
+            } else {
+                return {
+                    code: 200,
+                    success: false,
+                    message: "Can't delete default GPT Input/No GPT Input with such id exists!",
+                    data: null
+                };
+            }
+            
+        } catch (error) {
+            if(error.code === null || error.code === undefined){
+                error.code = 500;
+            }
+            return { code: error.code, success: false, data: error.data, message: error.message };
+        }
     };
 
     return this;
