345c8d8c9626774cb4234d864a7b2ced978638c7
dhanashri.p <dhanashri@intelehealth.org>
2023-06-08 12:11:07 +0530

    NAS-223 Complete visits count query changes

--- controllers/openMrs.controller.js
+++ controllers/openMrs.controller.js
@@ -1,45 +1,86 @@
 const openMrsDB = require("../public/javascripts/mysql/mysqlOpenMrs");
 
-const getVisitCountQuery = ({ speciality = "General Physician" }) => {
-  return `select count(t1.visit_id) as Total,
-  /*speciality,
-      state,*/
-     case
-    when  (encounter_type = 14 or encounter_type = 12 or com_enc = 1) then "Completed Visit"
-          when  (encounter_type = 9 ) then "Visit In Progress"
-          when (encounter_type) = 15 then "Priority"
-          when  ((encounter_type = 1 or encounter_type = 6) )  then "Awaiting Consult"
-       end as "Status"
-from encounter,
-(select 	v.visit_id,
-    v.patient_id,
-    max(case
-      when attribute_type_id = 5 then value_reference else null end) as "speciality",
-    max(case
-      when attribute_type_id = 6 then value_reference else null end) as "state"    ,
-    max(encounter_id) max_enc,
-          max(case when encounter_type = 14 then 1 else 0 end) as com_enc
-  from 	visit v,
-      visit_attribute va,
-      encounter e
-  where v.voided = 0
-      and	v.date_stopped is null
-  and va.voided = 0
-  and va.attribute_type_id in (5,6)
-  and va.visit_id = v.visit_id
-  and e.voided = 0
-  and e.visit_id = v.visit_id
-  group by 	v.visit_id,
-        v.patient_id
-  having max(case
-        when attribute_type_id = 5 then value_reference else null end) = "${speciality}") as t1
-where encounter_id = max_enc
+const getVisitCountQuery = () => {
+  return `select
+  count(t1.visit_id) as Total,
+  case
+  when (
+          sas_enc = 1
+      ) then "Completed Visit"
+      when (
+          encounter_type = 12
+          or com_enc = 1
+      ) then "ended_visits"
+      when (encounter_type = 9) then "Visit In Progress"
+      when (encounter_type) = 15 then "Priority"
+      when (
+          (
+              encounter_type = 1
+              or encounter_type = 6
+          )
+      ) then "Awaiting Consult"
+  end as "Status"
+from
+  encounter,
+  (
+      select
+          v.visit_id,
+          v.patient_id,
+          max(encounter_id) as max_enc,
+          max(
+              case
+                  when (encounter_type in (12, 14) or v.date_stopped is not null) then 1
+                  else 0
+              end
+          ) as com_enc,
+          max(
+              case
+                  when (encounter_type in (14) ) then 1
+                  else 0
+              end
+          ) as sas_enc,
+          max(
+              case
+                  when attribute_type_id = 5 then value_reference
+                  else null
+              end
+          ) as "speciality"
+      from
+          visit v
+          LEFT JOIN encounter e using (visit_id)
+          LEFT JOIN visit_attribute va on (va.visit_id= v.visit_id and va.voided = 0 and va.attribute_type_id = 5)
+      where
+          v.voided = 0
+          -- and date_stopped is null
+          and e.voided = 0
+      group by
+          v.visit_id,
+          v.patient_id
+  ) as t1
+where
+  encounter_id = max_enc
+  and (
+      speciality is null
+      or speciality = 'General Physician'
+  )   
 group by case
-    when  (encounter_type = 14 or encounter_type = 12 or com_enc = 1) then "Completed Visit"
-          when  (encounter_type = 9 ) then "Visit In Progress"
-          when (encounter_type) = 15 then "Priority"
-          when  ((encounter_type = 1 or encounter_type = 6) )  then "Awaiting Consult"
-       end;`;
+  when (
+          sas_enc = 1
+      ) then "Completed Visit"
+      when (
+          encounter_type = 12
+          or com_enc = 1
+      ) then "ended_visits"
+      when (encounter_type = 9) then "Visit In Progress"
+      when (encounter_type) = 15 then "Priority"
+      when (
+          (
+              encounter_type = 1
+              or encounter_type = 6
+          )
+      ) then "Awaiting Consult"
+  end
+order by 2;`;
 };
 
 const getVisitCountQueryForGp = () => {
@@ -85,10 +126,7 @@ group by case
  */
 const getVisitCounts = async (req, res, next) => {
   const speciality = req.query.speciality;
-  const query =
-    speciality === "General Physician"
-      ? getVisitCountQueryForGp()
-      : getVisitCountQuery({ speciality });
+  const query = getVisitCountQuery();
 
   try {
     const data = await new Promise((resolve, reject) => {
