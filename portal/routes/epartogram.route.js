57b11eeb91e1bd9025b550bb914062d984491c50
Zeeshan <zeeshan@intelehealth.org>
2024-04-26 18:42:07 +0530

    added auth to config APIs

--- portal/routes/epartogram.route.js
+++ portal/routes/epartogram.route.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { getConfiguration,addUpdateConfiguration } = require("../controllers/epartogram.controller");
+const authMiddleware = require("../middleware/auth");
 
-router.get("/configuration", getConfiguration);
-router.post("/configuration", addUpdateConfiguration);
+router.get("/configuration", [authMiddleware, getConfiguration]);
+router.post("/configuration", [authMiddleware, addUpdateConfiguration]);
 
 module.exports = router;
