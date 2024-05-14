477bd61d2174dbb061994215629b811a3f433346
dhaval-intelehealth <150244248+dhaval-intelehealth@users.noreply.github.com>
2024-05-14 15:31:59 +0530

    Abdm development master webapp 91 (#114)
    * feat: added api changes
    
    * feat: implemented the changes for login by abha number
    
    * fix: added error log
    
    * fix: Fixed the profile API issue
    
    * fix: added message to login via abha address
    
    * fix: added conditional validation
    
    * fix: visit route added
    
    * fix: added new abha user profile get url for healthid
    
    * fix: changed the status of validation failure issue
    
    ---------
    
    Co-authored-by: Dhaval <dhaval@intelehealth.com>

--- abha/middleware/validationMiddleware.js
+++ abha/middleware/validationMiddleware.js
@@ -1,15 +1,15 @@
 exports.validate = (schema) => (req, res, next) => {
-    const {
-      error
-    } = schema.validate(req.body);
-    if (error) {
-      res.json(
-            {
-                success: false,
-                code: 422,
-                message:  error.details[0].message,
-           });
-    } else {
-      next();
-    }
+  const {
+    error
+  } = schema.validate(req.body);
+  if (error) {
+    res.status(422).json(
+      {
+        success: false,
+        code: 422,
+        message: error.details[0].message,
+      });
+  } else {
+    next();
+  }
 };
