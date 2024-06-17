2ae35220778a797d6612d76d126090928031778a
Dhaval <dhaval@intelehealth.com>
2024-06-17 18:10:22 +0530

    fix: abha error handling

--- abha/handlers/errorHandller.js
+++ abha/handlers/errorHandller.js
@@ -2,15 +2,15 @@
 const errorHandler = (err, req, res, next) => {
 
   if (process.env.NODE_ENV !== "production") {
-    console.log(err);
+    // console.log(err);
   }
 
   res.status(err?.response?.status ?? err.status ?? 500);
 
   return res.json({
     success: false,
-    code: err.code || 422,
-    message: err?.response?.data?.message ?? err.message,
+    code: err.code ?? err?.response?.status ?? 422,
+    message: err?.response?.data?.error?.message ?? err?.response?.data?.message ?? err.message,
   });
 };
 
