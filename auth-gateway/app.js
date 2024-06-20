820536d117eb941fafe987613f7f4bf379c635db
Zeeshan <zeeshan@intelehealth.org>
2024-06-20 18:15:23 +0530

    WEBAPP-652

--- auth-gateway/app.js
+++ auth-gateway/app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const { authMiddleware } = require("./handlers/checkAuth");
 const indexRouter = require("./routes/index");
 const authRouter = require("./routes/auth");
@@ -33,6 +35,13 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/auth", authRouter);
 app.use("/v2", authMiddleware, indexRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+  explorer: true,
+  swaggerOptions: {
+    docExpansions: "none",
+    persistAuthorization: true
+  }
+}));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
