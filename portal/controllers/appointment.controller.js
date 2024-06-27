9946f801954be4265697c3d1c471f1c3096794f8
Zeeshan <zeeshan@intelehealth.org>
2024-06-27 12:47:49 +0530

    NAS-351

--- portal/controllers/appointment.controller.js
+++ portal/controllers/appointment.controller.js
@@ -354,7 +354,7 @@ module.exports = (function () {
         logStream('debug','Appointment Rescheduled', 'Reschedule Appointment');
         res.json({
           status: true,
-          ...data,
+          data,
         });
       }
     } catch (error) {
