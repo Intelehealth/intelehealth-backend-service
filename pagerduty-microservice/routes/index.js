af6c740a1ee54e75c21445019b761bd7bb4932c5
Dhaval <dhaval@intelehealth.com>
2024-07-10 14:35:53 +0530

    feat: added the webhook route for poc

--- pagerduty-microservice/routes/index.js
+++ pagerduty-microservice/routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.post('/webhook', function(req, res, next) {
+  console.log(JSON.stringify(req.body, null, 4));
+  return res.status(200).json(req.body);
+});
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+
+//ejN0ElTrk5GGH7aIDQyzl7ehFiTAb/JlFMxyEHqB+PdA9754prPXmlzZimejZaym
