72cd734fa70e05009a19d9e2c4eb7756496fe638
Vishal Majumdar <vishal@intelehealth.org>
2024-07-12 21:45:46 +0530

    Add CORS support to the application

--- portal/app.js
+++ portal/app.js
@@ -3,21 +3,23 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
+const cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var pushRouter = require("./routes/pushNotification");
 
 var app = express();
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+app.use(cors());
 
 app.use(logger("dev"));
 app.use(express.json({ limit: "50mb" }));
