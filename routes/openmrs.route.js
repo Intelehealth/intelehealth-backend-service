543f32a23eee868f05799dff223d8e00a4e83423
Zeeshan <zeeshan@intelehealth.org>
2024-07-15 15:50:21 +0530

    cleanup

--- routes/openmrs.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getVisitCounts,getFollowupVisits } = require("../controllers/openMrs.controller");
-
-router.get("/getVisitCounts", getVisitCounts);
-router.get("/getFollowupVisits", getFollowupVisits);
-
-module.exports = router;
