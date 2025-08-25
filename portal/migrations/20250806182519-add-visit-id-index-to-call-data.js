0531a558088b58d923cae6aef9ef2b22772fdfd0
Dhanashri694 <81160412+Dhanashri694@users.noreply.github.com>
2025-08-25 13:19:07 +0530

    Added migration file for AEAT-1828
    20250806182519-add-visit-id-index-to-call-data.js

---
+++ portal/migrations/20250806182519-add-visit-id-index-to-call-data.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    // Add index to visit_id column
+    await queryInterface.addIndex('call_data', ['visit_id'], {
+      name: 'call_data_visit_id'
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    // Remove index if rolling back
+    await queryInterface.removeIndex('call_data', ['visit_id']);
+    await queryInterface.removeIndex('call_data', 'call_data_visit_id');
+  }
+};
