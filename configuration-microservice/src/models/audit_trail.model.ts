7d8bc07e375088c046e9a00427babb749534e7c0
shaheen-p <spathan@intelehealth>
2025-09-17 13:25:30 +0530

    feat: updated languages as set in admin

--- configuration-microservice/src/models/audit_trail.model.ts
+++ configuration-microservice/src/models/audit_trail.model.ts
@@ -65,6 +65,7 @@ export class AuditTrail extends Model<AuditTrailAttributes, AuditTrailCreationAt
             'DROPDOWN CONFIG UPDATED',
             'HOME SCREEN SECTION NAME UPDATED',
             'HOME SCREEN SECTION ENABLED STATUS UPDATED',
+            'LANGUAGE STATUS UPDATED'
         ],
         allowNull: false
     })
