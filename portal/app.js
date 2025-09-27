6dc8597d318934bb31a65b91668d8ae76db4bf0b
satyadeep-ih <74664078+satyadeep-ih@users.noreply.github.com>
2025-09-27 14:11:57 +0530

    Update app.js

--- portal/app.js
+++ portal/app.js
@@ -9,7 +9,7 @@ const morganMiddleware = require("./middleware/morgan");
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const path =   require('path');
-const p = 'Ad';
+const p = 'A';
 const app = express();
 
 const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS && JSON.parse(process.env.ALLOWED_ORIGINS) || [];
