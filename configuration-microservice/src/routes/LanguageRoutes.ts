a26879c6470de476272d39f9d9f85faad9ef5984
shaheen-p <shaheen@intelehealth.org>
2025-10-08 18:33:16 +0530

    Update LanguageRoutes.ts

--- configuration-microservice/src/routes/LanguageRoutes.ts
+++ configuration-microservice/src/routes/LanguageRoutes.ts
@@ -34,22 +34,13 @@ async function setDefault(req: IReqUser, res: IRes) {
     await LanguageService.setDefault(id, userId, name);
     return res.status(HttpStatusCodes.OK).json({ success: true });
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/development_master
 async function getallEnabledLanguages(_: IReq, res: IRes) {
     const languages = await LanguageService.getAllEnabledLanguage();    
     return res.status(HttpStatusCodes.OK).json({ languages });
 }
-<<<<<<< HEAD
-
-async function updatePlatform(req: IReqUser<{ platform: string }>, res: IRes) {
-=======
 async function updatePlatform(req: IReqUser<{ platform: string }>, res: IRes) {
     console.log('updatePlatform called with body:', req.body);
     console.log('updatePlatform called with params:', req.params);
->>>>>>> origin/development_master
     const { id } = req.params;
     const { platform } = req.body; 
     const { userId, name } = req.user.data;
