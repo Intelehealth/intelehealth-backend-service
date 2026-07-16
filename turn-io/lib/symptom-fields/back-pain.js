645235c6934f4d58d5270436f87ec3176dac6afb
hatanvirintele <mhasan@intelehealth.org>
2026-07-16 16:15:40 +0600

    Backpain fields added

---
+++ turn-io/lib/symptom-fields/back-pain.js
@@ -0,0 +1,31 @@
+module.exports = [
+  ["start_time",                  "Since"],
+  ["onset",                       "Onset"],
+  ["location",                    "Location"],
+  ["character",                   "Character"],
+  ["severity",                    "Severity"],
+  ["pain_at_rest",                "Pain at rest"],
+  ["nocturnal_pain",              "Nocturnal pain"],
+  ["progression",                 "Progression"],
+  ["diurnal_variation",           "Diurnal variation"],
+
+  ["relieving_factors",           "Relieving factors"],
+  ["aggravating_factors",         "Aggravating factors"],
+  ["radiation",                   "Radiation"],
+  ["radiate_to",                  "Radiates to"],
+
+  ["assoc_weakness_lower_limbs",  "Associated: weakness of lower limbs"],
+  ["assoc_tingling_numbness",     "Associated: tingling/numbness"],
+  ["assoc_urine_incontinence",    "Associated: urine incontinence"],
+  ["assoc_urine_retention",       "Associated: urine retention"],
+  ["assoc_morning_stiffness",     "Associated: morning stiffness"],
+  ["assoc_injury",                "Associated: injury"],
+  ["assoc_limping",               "Associated: limping"],
+
+  ["other_symptoms",              "Other symptoms"],
+  ["treatment",                   "Treatment"],
+  ["treatment_details",           "Treatment details"],
+  ["additional",                  "Additional information"],
+
+  ["weight",                      "Weight"],
+];
--- turn-io/lib/symptom-fields/headache.js
+++ turn-io/lib/symptom-fields/headache.js
@@ -27,6 +27,7 @@ module.exports = [
   ["assoc_head_injury",        "Associated: head injury"],
 
   ["other_symptoms",           "Other symptoms"],
+  ["other_symptoms_detail",    "Other symptoms detailS"],
   ["treatment",                "Treatment"],
   ["treatment_details",        "Treatment details"],
   ["additional",               "Additional information"],
