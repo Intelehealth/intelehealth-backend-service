a3b93183be872b7fdfd079d178a524407f33b82a
Sagardoke2891 <112952750+Sagardoke2891@users.noreply.github.com>
2026-09-16 19:53:19 +0530

    Handle CORS preflight requests with OPTIONS method

--- portal/app.js
+++ portal/app.js
@@ -29,6 +29,9 @@ app.use(function (req, res, next) {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
+ if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
   next();
 });
 
